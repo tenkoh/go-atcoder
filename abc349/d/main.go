446b88ca1430244ce0861dae5c53a474790b8834
tenkoh <tenkoh.go@gmail.com>
2024-04-13 23:03:56 +0900

    abc349/d: わからなくて解説見てしまった。解説綺麗すぎる。

--- abc349/d/main.go
+++ abc349/d/main.go
@@ -11,48 +11,19 @@ type s struct {
 	l, r int
 }
 
-// nを超えない最大の2のべき乗を返す
-func pow2(n int) int {
-	res := 1
-	for res*2 <= n {
-		res *= 2
+// see: https://atcoder.jp/contests/abc349/editorial/9788
+func f(l, r, L, R int) []s {
+	if L <= l && r <= R {
+		return []s{{l, r}}
 	}
-	return res
-}
-
-func solve(whole s) []s {
-	l := whole.l
-	r := l + 1
-	ans := []s{}
-	for r <= whole.r {
-		// 左端が奇数の場合はj=l, i=0。r=j+1
-		if l%2 == 1 {
-			ans = append(ans, s{l, r})
-			l = r
-			r = l + 1
-			continue
-		}
-
-		// 左端が0の場合はj=0. rは行けるだけ行く
-		if l == 0 {
-			pow := pow2(whole.r)
-			ans = append(ans, s{0, pow})
-			l = pow
-			r = l + 1
-			continue
-		}
-
-		pow := pow2(l)
-		if whole.r-l < pow {
-			pow = pow2(whole.r - l)
-		}
-		j := l / pow
-		r = pow * (j + 1)
-		ans = append(ans, s{l, r})
-		l = r
-		r = l + 1
+	m := (l + r) / 2
+	if R <= m {
+		return f(l, m, L, R)
 	}
-	return ans
+	if m <= L {
+		return f(m, r, L, R)
+	}
+	return append(f(l, m, L, R), f(m, r, L, R)...)
 }
 
 func main() {
@@ -63,7 +34,7 @@ func main() {
 	l, _ := strconv.Atoi(sc.Text())
 	sc.Scan()
 	r, _ := strconv.Atoi(sc.Text())
-	areas := solve(s{l: l, r: r})
+	areas := f(0, 1<<60, l, r)
 	fmt.Println(len(areas))
 	bw := bufio.NewWriter(os.Stdout)
 	for _, v := range areas {
